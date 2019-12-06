28e1637b99ac1ddb723fc66dbfbe1c6cda4367bf
Vulner-6 <1141613511@qq.com>
2019-12-06 21:45:13 +0800

    2019.12.6

--- src/main/java/life/majiang/community/controller/HelloController.java
+++ src/main/java/life/majiang/community/controller/HelloController.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController
 {
-    @GetMapping("/hello")
+    @GetMapping("/")
     //获取浏览器过来的请求名为user的参数值，存到变量name中
     public String hello(@RequestParam(name = "user", required=false, defaultValue="World") String name, Model model)
     {
         //将上面变量name中的值，作为model中名为"name"的key的值
         model.addAttribute("name",name);
         //调用templates中，名为hello的html文件，进行渲染
-        return "hello";
+        return "index";
     }
 }
