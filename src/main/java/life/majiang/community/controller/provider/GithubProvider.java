3b27faee70c28aa94117c73901c4e850d2c968cf
Vulner-6 <1141613511@qq.com>
2019-12-08 23:02:04 +0800

    2019.12.8

--- src/main/java/life/majiang/community/controller/provider/GithubProvider.java
+++ src/main/java/life/majiang/community/controller/provider/GithubProvider.java
@@ -2,14 +2,8 @@ package life.majiang.community.controller.provider;
 
 import com.alibaba.fastjson.JSON;
 import life.majiang.community.controller.dto.AccessTokenDTO;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import okhttp3.*;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestBody;
-import okhttp3.MediaType.Companion.toMediaTypeOrNull;
-import okhttp3.RequestBody.Companion.asRequestBody;
 
 @Component
 public class GithubProvider
@@ -22,11 +16,11 @@ public class GithubProvider
      */
     public String getAccessToken(AccessTokenDTO accessTokenDTO)
     {
-         MediaType mediaType = MediaType.Companion.parse("application/json; charset=utf-8");
+         MediaType mediaType = MediaType.get("application/json; charset=utf-8");
 
         OkHttpClient client = new OkHttpClient();
 
-        RequestBody body = RequestBody.Companion.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
         Request request = new Request.Builder()
                 .url("https://github.com/login/oauth/access_token")
                 .post(body)
